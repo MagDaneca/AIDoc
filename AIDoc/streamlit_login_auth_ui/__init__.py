43344821cae7d8bb0f0a95f70a6e950d44e5bca7
Yordan Andronov <105605157+MagDaneca@users.noreply.github.com>
2024-02-16 19:36:23 +0200

    Delete AIDoc directory

--- AIDoc/streamlit_login_auth_ui/__init__.py
+++
@@ -1,4 +0,0 @@
-from streamlit_login_auth_ui.widgets import __login__
-
-# Author: Gauri Prabhakar
-# GitHub: https://github.com/GauriSP10/streamlit_login_auth_ui
--- AIDoc/utils/database.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-def create_connection():
-    conn = sqlite3.connect(r'C:\Users\MSI\Desktop\AIDoc\aidoc.db')
-    return conn
