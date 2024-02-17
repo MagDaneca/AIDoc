e5b63492aadba061e09d645ddeed195eaa6e60fd
dmish66 <106017885+dmish66@users.noreply.github.com>
2024-02-17 23:41:30 +0200

    Update app.py

--- AIDoc/app.py
+++ AIDoc/app.py
@@ -292,7 +292,7 @@ if LOGGED_IN == True:
         if selected == 'Заболяване на черния дроб':
             age, TotalBilirubin, DirectBilirubin, AlkalinePhosphatase, AlanineAminotransferase, AspartateAminotransferase, TotalProtein, Albumin, AlbuminAndGlobulinRatio = get_data_liver(conn, username)
 
-            col1, col2, col3  = st.columns([0.5,1,0.5])
+            col1, col2, col3  = st.columns([1,1,0.5])
             col4,col5 = st.columns([1.6,0.1])
             col6,col7,col8 = st.columns([0.5,1,0.5])
             col9,col10 = st.columns([1.6,0.1])
