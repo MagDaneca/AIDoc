c18648755e2cd0ebf0ebc381dbf9fe3f2b91ebd2
Yordan Andronov <105605157+MagDaneca@users.noreply.github.com>
2024-04-23 17:45:19 +0300

    Update app.py

--- AIDoc/app.py
+++ AIDoc/app.py
@@ -98,9 +98,9 @@ if LOGGED_IN == True:
         option3 = 'Тествай се'
         option4 = 'Намери лекарство'
         with st.sidebar:
+            st.image('AIDoc/logo.png', width = 200)
             container = st.container (border=True)
             with container:
-                st.image('AIDoc/logo.png', width = 200)
                 selected = option_menu('AIDoc',
                                 
                                 [
