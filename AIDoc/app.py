f6eb6bef79d4dece585f1a89e331c63d0a408f39
MagDaneca <netherbull@gmail.com>
2025-03-10 20:19:24 +0200

    Update app.py

--- AIDoc/app.py
+++ AIDoc/app.py
@@ -70,7 +70,7 @@ diabetes_model = pickle.load(open(rb'AIDoc/Trained models/diabetes_model_medassi
 liver_disease_model = pickle.load(open(rb'AIDoc/Trained models/liver_disease_model.sav', 'rb'))
 heart_disease_model = pickle.load(open(rb'AIDoc/Trained models/heart_disease_model.sav','rb'))
 
-__login__obj = __login__(auth_token = "pk_prod_TSABQG9T12M5J7P5Q91W4CK2HN75", 
+__login__obj = __login__(auth_token = "pk_prod_G3NNN8WW8J4NSEK33M5Y1FK88TM0", 
                          
                     company_name = "AIDoc",
                     width = 200, height = 250, 
